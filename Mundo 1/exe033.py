6b4a80e9d4cb17708e033f84193e9d4fdaed6e40
Jordania <jordaniagabriellematoscorrea@gmail.com>
2025-06-02 22:21:13 -0300

    add exe 033

--- Mundo 1/exe033.py
+++ Mundo 1/exe033.py
@@ -1,3 +1,23 @@
 # Faça um programa que leia três números e mostre qual é o maior e qual é o manor.
 
+lista = []
+ 
+while True: 
 
+    numero = input('Digite o número: (Para parar digite "N") ')
+
+    if numero != "N":
+        try:
+            n = float(numero)
+            lista.append(n)
+
+        except ValueError:
+            print('Voce deve inserir somente números ')
+    else:
+        break
+    
+if lista:
+    print('O maior número é: ', max(lista))
+    print('O menor número é: ', min(lista))
+else:
+    print('Nenhum número foi add')
